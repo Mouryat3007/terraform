a60c6e545e26ee84307e565b5926fa9b1d244a26
mouryat3007 <mouryat3007@gmail.com>
2024-07-11 17:46:51 +0100

    vpc and subnet

---
+++ vpc.tf
@@ -0,0 +1,41 @@
+
+# vpc
+resource "aws_vpc" "crm-vpc" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "crm-vpc"
+  }
+}
+
+
+#websubnet
+resource "aws_subnet" "web-sn" {
+  vpc_id     = aws_vpc.crm-vpc.id
+  cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = "true"
+
+  tags = {
+    Name = "web-subnet"
+  }
+}
+#apisubnet
+resource "aws_subnet" "api-sn" {
+  vpc_id     = aws_vpc.crm-vpc.id
+  cidr_block = "10.0.2.0/24"
+  map_public_ip_on_launch = "true"
+
+  tags = {
+    Name = "api-subnet"
+  }
+}
+#databasesubent
+resource "aws_subnet" "db-sn" {
+  vpc_id     = aws_vpc.crm-vpc.id
+  cidr_block = "10.0.3.0/24"
+
+  tags = {
+    Name = "database-subnet"
+  }
+}
