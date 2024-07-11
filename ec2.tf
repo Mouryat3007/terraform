0bf36c526bfe93fbab2732a115818c1f01dace2e
mouryat3007 <mouryat3007@gmail.com>
2024-07-11 19:15:22 +0100

    updated sg

--- ec2.tf
+++ ec2.tf
@@ -2,10 +2,10 @@
 
 resource "aws_instance" "crm-web-server" {
   ami           = "ami-07c8c1b18ca66bb07"
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
   subnet_id = aws_subnet.web-sn.id
   key_name = "mour"
-  vpc_security_group_ids = aws_security_group.crm-web-sg.id
+  vpc_security_group_ids = [aws_security_group.crm-web-sg.id]
   user_data = file("setup.sh")
 
 
